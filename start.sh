2d38c196451666e557f6f6d1544d51f86d5b6a6c
yzqiang666 <yzqiang6@gmail.com>
2021-01-11 10:51:23 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 cd /app
 curl -o gost http://ggcloud.tk/mac/gost
 chmod a+x gost
-./gost -L=ss+mws://chacha20:ss123456@:$TLS_PORT &
-date>>gost.log
-node server.js -b 0.0.0.0
+./gost -L=ss+mws://chacha20:ss123456@:$TLS_PORT >>/dev/null 2>>&1&
+#date>>gost.log
+#node server.js -b 0.0.0.0
 #npm start 
